120371eb895e7259177eb17c9a817ed86360d596
PedroVMB <pedromb.dev@gmail.com>
2023-11-30 17:12:58 -0300

    fix: Corrigindo erro de cadastro e de listagem dos dados, alterando para formato JSON ao invés de txt. Alterando caminho de respostas e porta do servidor. Corrijindo erro na classe principal do professor

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,21 @@ import Controller.tests.SecondHandler;
 import com.sun.net.httpserver.HttpServer;
 import java.net.InetSocketAddress;
 import java.io.IOException;
+import Controller.TeacherController;
+import Service.TeacherService;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(3001), 0);
+        TeacherService teacherService = new TeacherService();
+        TeacherController teacherController = new TeacherController(teacherService);
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(3000), 0);
         server.createContext("/api", new MyHandler());
         server.createContext("/api/teste", new SecondHandler());
-        server.setExecutor(null); // Use o executor padrão
+
+        server.createContext("/api/teachers", teacherController);
+        server.setExecutor(null);
         server.start();
+        System.out.println("Servidor HTTP iniciado na porta 3000");
     }
 }
