ed495a7470259815c9b6a889be62e54cee265c32
PedroVMB <pedromb.dev@gmail.com>
2023-12-01 09:11:57 -0300

    feat: adicionando controllador da discipline e incluindo o enpoint na main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import Controller.DisciplineController;
 import Controller.tests.MyHandler;
 import Controller.tests.SecondHandler;
+import Service.DisciplineService;
 import com.sun.net.httpserver.HttpServer;
 import java.net.InetSocketAddress;
 import java.io.IOException;
@@ -11,11 +13,16 @@ public class Main {
         TeacherService teacherService = new TeacherService();
         TeacherController teacherController = new TeacherController(teacherService);
 
+        DisciplineService disciplineService = new DisciplineService();
+        DisciplineController disciplineController = new DisciplineController(disciplineService);
+
         HttpServer server = HttpServer.create(new InetSocketAddress(3000), 0);
         server.createContext("/api", new MyHandler());
         server.createContext("/api/teste", new SecondHandler());
 
         server.createContext("/api/teachers", teacherController);
+
+        server.createContext("/api/disciplines", disciplineController);
         server.setExecutor(null);
         server.start();
         System.out.println("Servidor HTTP iniciado na porta 3000");
