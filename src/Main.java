7e8486e44c066697fbd95fd3548c99331944352b
PedroVMB <pedromb.dev@gmail.com>
2023-12-04 13:43:23 -0300

    feat: adicionando controller do classroom e alterando sql da consulta

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import Controller.ClassroomController;
 import Controller.DisciplineController;
 import Controller.tests.MyHandler;
 import Controller.tests.SecondHandler;
+import Service.ClassroomService;
 import Service.DisciplineService;
 import com.sun.net.httpserver.HttpServer;
 import java.net.InetSocketAddress;
@@ -16,13 +18,18 @@ public class Main {
         DisciplineService disciplineService = new DisciplineService();
         DisciplineController disciplineController = new DisciplineController(disciplineService);
 
+        ClassroomService classroomService = new ClassroomService();
+        ClassroomController classroomController = new ClassroomController(classroomService);
+
         HttpServer server = HttpServer.create(new InetSocketAddress(3000), 0);
-        server.createContext("/api", new MyHandler());
-        server.createContext("/api/teste", new SecondHandler());
 
         server.createContext("/api/teachers", teacherController);
 
         server.createContext("/api/disciplines", disciplineController);
+
+        server.createContext("/api/classrooms", classroomController);
+
+
         server.setExecutor(null);
         server.start();
         System.out.println("Servidor HTTP iniciado na porta 3000");
